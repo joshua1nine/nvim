9bf0368dd8e8d721e9cdb765689e0a160992b3ef
Joshua Michael <joshua.kale.michael@gmail.com>
2022-08-05 10:57:13 -0500

    Add tailwindcss Lsp

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -1,1 +1,2 @@
-vim.cmd("let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md', 'nested_syntaxes': {'js': 'javascript', 'md': 'markdown'}}]")
+-- vim.cmd("let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md', 'nested_syntaxes': {'js': 'javascript', 'md': 'markdown'}}]")
+vim.cmd("let g:vimwiki_list = [{'path': '~/vimwiki/', 'nested_syntaxes': {'js': 'javascript', 'md': 'markdown'}}]")
