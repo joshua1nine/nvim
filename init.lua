4115373dd40fb66aac9089fc0d3f9a24971eb216
Joshua <joshua.kale.michael@okstate.edu>
2025-01-23 08:51:36 -0600

    Reformat

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require("keymaps")
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-haighlight-yank', {clear = ture}),
+	group = vim.api.nvim_create_augroup('kickstart-haighlight-yank', {clear = true}),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
