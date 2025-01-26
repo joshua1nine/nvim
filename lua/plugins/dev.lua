34f31fa652c57a63c02bc13d99b8c8d312803064
Joshua Michael <joshua.kale.michael@gmail.com>
2025-01-25 19:19:08 -0600

    Add lg plugin

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -3,12 +3,12 @@ return {
       dir = "~/Repos/plugins/present.nvim",
       dev = { true },
    },
-   {
-      dir = "~/Repos/plugins/lg.nvim",
-      dev = { true },
-      opts = {},
-      keys = {
-         { "<leader>lg", "<cmd>LgToggle<cr>", desc = "Toggle Lazygit" },
-      }
-   },
+   -- {
+   --    dir = "~/Repos/plugins/lg.nvim",
+   --    dev = { true },
+   --    opts = {},
+   --    keys = {
+   --       { "<leader>lg", "<cmd>LgToggle<cr>", desc = "Toggle Lazygit" },
+   --    }
+   -- },
 }
---
+++ lua/plugins/lg.lua
@@ -0,0 +1,9 @@
+return {
+   {
+      "joshua1nine/lg.nvim",
+      opts = {},
+      keys = {
+         { "<leader>lg", "<cmd>LgToggle<cr>", desc = "Toggle Lazygit" },
+      }
+   },
+}
