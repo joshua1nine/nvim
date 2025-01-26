67167d2ba2163a4b94f7574257148b13934c609e
Joshua Michael <joshua.kale.michael@gmail.com>
2025-01-25 19:19:25 -0600

    Update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,14 @@
 return {
-   {
-      "stevearc/oil.nvim",
-      dependencies = { { "echasnovski/mini.icons", opts = {} } },
-      config = function()
-         require("oil").setup()
-         vim.keymap.set("n", "<leader>e", require("oil").toggle_float, { desc = "Open parent directory" })
-      end,
-   },
+	{
+		"stevearc/oil.nvim",
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		config = function()
+			require("oil").setup({
+            view_options = {
+               show_hidden = true
+            }
+			})
+			vim.keymap.set("n", "<leader>e", require("oil").toggle_float, { desc = "Open parent directory" })
+		end,
+	},
 }
