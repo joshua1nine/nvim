179f32c60fae5163aa7fa18e90726b2ab5dcc05c
Joshua <joshua.kale.michael@okstate.edu>
2025-01-30 15:19:05 -0600

    Improve nvim tree mappings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,7 +7,30 @@ return {
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+
+         local openShut = function ()
+			   api.node.open.edit()
+            api.tree.close()
+         end
+
+			-- custom mappings
+			vim.keymap.set("n", "l", openShut, opts("Open File") )
+			vim.keymap.set("n", "<CR>", openShut, opts("Open File") )
+			vim.keymap.set("n", "o", openShut, opts("Open File") )
+
+		end
+
 		nvimtree.setup({
+			on_attach = my_on_attach,
 			view = {
 				width = 35,
 			},
@@ -16,7 +39,7 @@ return {
 					window_picker = {
 						enable = false,
 					},
-				}
+				},
 			},
 			filters = {
 				custom = { ".DS_Store" },
