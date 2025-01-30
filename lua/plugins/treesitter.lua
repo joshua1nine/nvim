becc468d95dcb5d5531c579bc050170fbde9843c
Joshua <joshua.kale.michael@okstate.edu>
2025-01-30 15:18:39 -0600

    Add autotag via treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,39 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
-        highlight = { enable = true },
-        indent = { enable = true },
-        textobjects = {
-          select = {
-            enable = false
-          }
-        }
-      })
-    end,
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = { "windwp/nvim-ts-autotag" },
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"typescript",
+					"html",
+					"php",
+					"json",
+					"css",
+					"yaml",
+					"tsx",
+					"markdown",
+					"bash",
+               "gitignore"
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+				autotag = { enable = true },
+				textobjects = {
+					select = {
+						enable = false,
+					},
+				},
+			})
+		end,
+	},
 }
