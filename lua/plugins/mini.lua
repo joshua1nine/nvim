3d8804f7b5f9418d0805ba6d253a5dd2ba83a62b
Joshua <joshua.kale.michael@okstate.edu>
2025-01-30 15:16:14 -0600

    replace mini comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,16 +4,12 @@ return {
     version = '*',
     enabled = true,
     config = function()
-      require('mini.statusline').setup({
-        use_icons = true
-      })
       require('mini.ai').setup()
       require('mini.surround').setup()
       require('mini.pairs').setup()
       require('mini.bracketed').setup()
       require('mini.starter').setup()
-      require('mini.comment').setup()
-      -- require('mini.tabline').setup()
+      -- require('mini.comment').setup()
     end
   }
 }
